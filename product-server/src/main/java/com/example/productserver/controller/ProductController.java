daf09b171fa4cf33c686a68e703a80786a5cf01e
747Sgr <shiguorang@163.com>
2021-09-06 22:14:07 +0800

    table

---
+++ product-server/src/main/java/com/example/productserver/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.example.productserver.controller;
+
+import com.example.productserver.entity.Product;
+import com.example.productserver.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * (Product)表控制层
+ *
+ * @author makejava
+ * @since 2021-09-06 21:20:07
+ */
+@RestController
+@RequestMapping("product")
+public class ProductController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private ProductService productService;
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("selectOne")
+    public Product selectOne(long id) {
+        return this.productService.queryById(id);
+    }
+
+}
---
+++ product2-server/src/main/java/com/example/productserver/entity/Product.java
@@ -0,0 +1,51 @@
+package com.example.productserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * (Product)实体类
+ *
+ * @author makejava
+ * @since 2021-09-06 21:25:35
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Product implements Serializable {
+    private static final long serialVersionUID = -65343577356993838L;
+
+    private Long id;
+
+    private Date createAt;
+
+    private String createBy;
+
+    private Date updateAt;
+
+    private String updateBy;
+
+    private String productCode;
+
+    private String productName;
+    /**
+     * 价格
+     */
+    private Double price;
+    /**
+     * 单位
+     */
+    private String unit;
+    /**
+     * 备注
+     */
+    private String remarks;
+
+
+}
